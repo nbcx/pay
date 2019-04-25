155af6857be61b48c2d433f52067fb7f228038d6
Collin <collin@nb.cx>
2019-04-25 19:02:29 +0800

    init

---
+++ src/IPay.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the NB Framework package.
+ *
+ * Copyright (c) 2018 https://nb.cx All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace nbcx\pay;
+
+/**
+ * IPay
+ *
+ * @link https://nb.cx
+ * @author: collin <collin@nb.cx>
+ * @date: 2019/4/25
+ */
+abstract class IPay {
+
+    public function index() {
+
+    }
+
+}
